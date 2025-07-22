6ff11f94427df4aeaa5ce5196f32dea9bf46cb45
ankit <ce21btech11057@iith.ac.in>
2025-07-22 18:26:11 +0530

    dgfb

---
+++ Dockerfile_1.py
@@ -0,0 +1,21 @@
+# Use official PyTorch image
+FROM pytorch/pytorch:2.2.2-cuda12.1-cudnn8-runtime
+
+# Set working directory
+WORKDIR /app
+
+# Install required packages
+RUN pip install --no-cache-dir \
+    torch \
+    torchvision \
+    tqdm \
+    kubernetes\
+    kubeflow-katib
+
+# Copy training script
+COPY objective_fn.py .
+
+
+# Default command for Katib trial execution
+ENTRYPOINT ["python", "objective_fn.py"]
+
